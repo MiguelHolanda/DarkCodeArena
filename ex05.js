133100f5e9c51370077ad7147ba803c9180dfc04
miguelalmeidaholanda@gmail.com <miguelalmeidaholanda@gmail.com>
2026-05-12 14:51:25 -0300

    subindo ex05

---
+++ ex05.js
@@ -0,0 +1,12 @@
+console.log("")
+console.log("________________________________________")
+console.log("")
+console.log("    CONTADOR DO 5 ao 25")
+console.log("________________________________________")
+console.log("")
+for(let i = 5; i<26; i++){
+    console.log(i)
+    console.log("~~~")
+}
+console.log("")
+console.log("________________________________________")
