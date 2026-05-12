a92f19a9dd8d506eb6a3bd0f1419aa920fc6392b
miguelalmeidaholanda@gmail.com <miguelalmeidaholanda@gmail.com>
2026-05-12 14:46:55 -0300

    subindo ex04

---
+++ ex04.js
@@ -0,0 +1,11 @@
+console.log("")
+console.log("________________________________________")
+console.log("")
+console.log("    CONTADOR DO 1 ao 10")
+console.log("________________________________________")
+console.log("")
+for(let i = 1; i<11; i++){
+    console.log(i)
+    console.log("~~~")
+}
+console.log("")
