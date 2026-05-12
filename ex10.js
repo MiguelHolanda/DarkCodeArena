7ef4f5a70975086171da1f840af6a6e5926c80c8
miguelalmeidaholanda@gmail.com <miguelalmeidaholanda@gmail.com>
2026-05-12 17:06:28 -0300

    subindo ex10

---
+++ ex10.js
@@ -0,0 +1,17 @@
+let soma =0
+console.log("")
+console.log("________________________________________")
+console.log("")
+console.log("      ~~SOMADOR DE  NUMEROS PARES")
+console.log("________________________________________")
+console.log("")
+for(let i = 2; i<101; i+=2){
+    soma = soma + i
+}
+console.log("  A SOMA DE TODOS OS NUMEROS PARES ENTRE 1 e 100 É; ")
+console.log(`                                          ~~~~~${soma}`)
+
+console.log("")
+console.log("________________________________________")
+console.log("")
+
